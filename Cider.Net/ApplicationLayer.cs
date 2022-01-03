d0c05fee9d8947e66b93a7023cce6ff7c10a3106
金路超 <785515163@qq.com>
2022-01-03 16:49:35 +0800

    完成了应用层通信类实现

--- Cider.Net/ApplicationLayer.cs
+++ Cider.Net/ApplicationLayer.cs
@@ -38,6 +38,49 @@ namespace Cider.Net
             Option = (byte)opion;
         }
 
+        /// <summary>
+        /// 将头部信息转化为字节序列
+        /// </summary>
+        public byte[] GetBytes()
+        {
+            byte[] byteArray = new byte[5];
+            byteArray[0] = Option;
+            // 小端法存储长度信息
+            // 低字节存放低位数据
+            uint length = DataLength;
+            for (int i = 0; i < 4; i++)
+            {
+                byteArray[i + 1] = (byte)(length & 0xFF);
+                length >>= 8;    // 下一个高位字节
+            }
+
+            return byteArray;
+        }
+
+        /// <summary>
+        /// 从字节数组中读取头部信息
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        public static ApplicationHead CreateFromBytes(byte[] bArray)
+        {
+            ApplicationHead head = new ApplicationHead(); 
+            if (bArray.Length >= 5)
+            {
+                head.Option = bArray[0];
+                head.DataLength = 0;
+                for (int i = 4; i > 0; i--)
+                {
+                    head.DataLength |= bArray[i];
+                    head.DataLength <<= 8;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("需要的数组长度至少为5");
+            }
+
+            return head;
+        }
     }
 
     public abstract class ApplicationLayer
@@ -46,20 +89,44 @@ namespace Cider.Net
 
         public abstract void SetHead(ApplicationOption option);
 
+        /// <summary>
+        /// 发送比特流
+        /// </summary>
         public abstract void Send(byte[] data);
 
+        /// <summary>
+        /// 发送文件分块的哈希值列表
+        /// </summary>
         public abstract void SendHashList(string[] hashs);
 
+        /// <summary>
+        /// 发送返回的文件块数
+        /// </summary>
         public abstract void SendReturnNumber(int number);
 
+        /// <summary>
+        /// 发送线性表达式计算结果
+        /// </summary>
         public abstract void SendLinearResult(byte[] data);
 
+        /// <summary>
+        /// 接收比特流
+        /// </summary>
         public abstract int Receive(byte[] data);
 
+        /// <summary>
+        /// 接收哈希列表
+        /// </summary>
         public abstract string[] ReceiveHashList();
 
+        /// <summary>
+        /// 接收返回的文件块数
+        /// </summary>
         public abstract int ReceiveReturnNumber();
 
+        /// <summary>
+        /// 接收线性表达式计算结果
+        /// </summary>
         public abstract byte[] ReceiveLinearResult();
 
     }
--- Cider.Net/ApplicationService.cs
+++ Cider.Net/ApplicationService.cs
@@ -7,49 +7,111 @@ using System.Threading.Tasks;
 
 namespace Cider.Net
 {
-    public class ApplicationService : ApplicationLayer
+    public class ApplicationService : ApplicationLayer, IDisposable
     {
+        #region Field
+
         protected TcpClient client;
+
+        protected NetworkStream nStream;
+
+        #endregion
+
+        #region Property
 
         public override ApplicationHead Head { get; protected set; }
 
-        public ApplicationService()
+        /// <summary>
+        /// 编码方式
+        /// </summary>
+        public static Encoding? EnCode { get; set; }
+
+        /// <summary>
+        /// 哈希字段的长度
+        /// </summary>
+        public static int HashLength { get; set; }
+
+        /// <summary>
+        /// 服务是否可使用
+        /// </summary>
+        public static bool CanProvideService
         {
-            Head = new ApplicationHead();
-            client = new TcpClient();
+            get
+            {
+                return (EnCode is not null) && (HashLength != 0);
+            }
+        }
+
+        /// <summary>
+        /// 是否有数据可读
+        /// </summary>
+        protected bool HasData
+        {
+            get
+            {
+                return nStream.DataAvailable;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public ApplicationService() : this(new TcpClient())
+        {
         }
 
         public ApplicationService(TcpClient tcp)
         {
             Head = new ApplicationHead();
             client = tcp;
+            nStream = client.GetStream();
         }
+
+        #endregion
+
+        #region Method
 
         public override int Receive(byte[] data)
         {
-            var stream = client.GetStream();
-            return stream.Read(data);
+            if (nStream.CanRead)
+                return nStream.Read(data);
+            else
+                return 0;
         }
 
         public override void Send(byte[] data)
         {
-            var stream = client.GetStream();
-            stream.Write(data, 0, data.Length);
+            if (nStream.CanWrite)
+                nStream.Write(data, 0, data.Length);
         }
 
         public override void SendHashList(string[] hashs)
         {
-            MemoryStream ms = new MemoryStream();
+            Head.SetOption(ApplicationOption.SendHashList);
+            Head.DataLength = (uint)hashs.Length;
+            Send(Head.GetBytes());
+            foreach(string str in hashs)
+            {
+                var buf = Encoding.ASCII.GetBytes(str); // 哈希字符串用ASCII编码传输
+                Send(buf);
+            }
         }
 
         public override void SendReturnNumber(int number)
         {
-            throw new NotImplementedException();
+            Head.SetOption(ApplicationOption.ReturnNumber);
+            Head.DataLength = 1U;
+            Send(Head.GetBytes());
+            Send(ToBytes(number));
         }
 
         public override void SendLinearResult(byte[] data)
         {
-            throw new NotImplementedException();
+            Head.SetOption(ApplicationOption.SendLinearResult);
+            Head.DataLength = (uint)data.Length;
+            Send(Head.GetBytes());
+            Send(data);
         }
 
         public override void SetHead(ApplicationOption option)
@@ -59,17 +121,124 @@ namespace Cider.Net
 
         public override string[] ReceiveHashList()
         {
-            throw new NotImplementedException();
+            if (HashLength != 0)
+                throw new ArgumentException("HashLength is not init.");
+
+            byte[] bhead = new byte[5];
+            if (Receive(bhead) != 5)
+                throw new Exception();  // 收到的头部字节不完整
+
+            ApplicationHead head = ApplicationHead.CreateFromBytes(bhead);
+            if (head.Option != (byte)ApplicationOption.SendHashList)
+                throw new Exception();  // 收到的数据报不是该操作
+
+            string[] hashs = new string[head.DataLength];   // 哈希列表
+            byte[] buf = new byte[HashLength << 1];     // 哈希一个字节用两个十六进制显示的字符表示
+                                                        // 一个ASCII字符占一个字节
+            int i = 0,  // 计数 已完整收到的哈希数量
+                waitTimes = 0;  // 等待次数
+            using MemoryStream mStream = new MemoryStream();
+            while (i < head.DataLength)     // 需要接收到报头给定的哈希的数量才停止
+            {
+                int count = Receive(buf);   // 一次接收的字节数
+                mStream.Write(buf, 0, count);   // 接收的字节写入内存缓冲区
+                if (mStream.Length >= buf.Count())  // 已接收到一个哈希对应的字节数量
+                {
+                    mStream.Read(buf, 0, buf.Count());  // 读取出字节流
+                    hashs[i] = Encoding.ASCII.GetString(buf);   // 转化为哈希字符串
+                    waitTimes = 0;  // 重置等待次数
+                    i++;    // 累加
+                }
+                else if (waitTimes > 10     // 等待次数超过10次 认为网络异常 不再等待
+                      || count == 0)        // 返回字节为0 远程主机关闭了连接                                 
+                {
+                    throw new Exception();  // 接收到的哈希字段数量有误
+                }
+                else    // 未读满一个哈希对应的字节数量
+                {
+                    Thread.Sleep(1000); // 等待1s
+                    waitTimes++;
+                }
+            }
+
+            return hashs;
         }
 
         public override int ReceiveReturnNumber()
         {
-            throw new NotImplementedException();
+            byte[] buf = new byte[9];
+            if (Receive(buf) != 9)
+                throw new Exception();  // 收到信息不完整
+            var head = ApplicationHead.CreateFromBytes(buf);
+            if (head.Option != (byte)ApplicationOption.ReturnNumber)
+                throw new Exception();  // 收到的数据报不是该操作
+            
+            return ToInt(buf, 5);
         }
 
         public override byte[] ReceiveLinearResult()
         {
-            throw new NotImplementedException();
+            byte[] bhead = new byte[5];
+            if (Receive(bhead) != 5)
+                throw new Exception();  // 收到的头部字节不完整
+
+            ApplicationHead head = ApplicationHead.CreateFromBytes(bhead);
+            if (head.Option != (byte)ApplicationOption.SendLinearResult)
+                throw new Exception();  // 收到的数据报不是该操作
+            
+            byte[] buf = new byte[1024];    // 缓冲区
+            using MemoryStream mStream = new MemoryStream();
+            int waitTimes = 0;  // 等待次数
+            while (mStream.Length < head.DataLength)
+            {
+                if (HasData)    // 有数据可读取
+                {
+                    int count = Receive(buf);
+                    mStream.Write(buf, 0, count);
+                    waitTimes = 0;  // 重置等待次数
+                }
+                else if (waitTimes <= 10)   // 收到
+                {
+                    Thread.Sleep(1000); // 等待1s
+                    waitTimes++;
+                }
+                else    // 等待超过10次 不再等待
+                {
+                    throw new Exception();  // 收到的字节数有误
+                }
+            }
+
+            return mStream.GetBuffer();
         }
+
+        public void Dispose()
+        {
+            nStream.Close();
+            client.Close();
+        }
+
+        protected static byte[] ToBytes(int number)
+        {
+            byte[] bs = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                bs[i] = (byte)(number & 0xFF);
+                number >>= 8;
+            }
+            return bs;
+        }
+
+        protected static int ToInt(byte[] bs, int offset)
+        {
+            int number = 0;
+            for (int i = offset; i < offset + 4; i++)
+            {
+                number |= bs[i];
+                number <<= 8;
+            }
+            return number;
+        }
+
+        #endregion
     }
 }
