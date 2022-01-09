1818727996d32578c07847abfc89cc67f03d097e
金路超 <785515163@qq.com>
2022-01-09 23:50:50 +0800

    测试项目

---
+++ Cider.ServerTests/CommunicateServerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cider.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cider.Tests
+{
+    [TestClass()]
+    public class CommunicateServerTests
+    {
+        [TestMethod()]
+        public void CommunicateServerTest()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void CommunicateServerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RunTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Cider.ServerTests/VandermondeEnumeratorTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cider.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cider.Math.Tests
+{
+    [TestClass()]
+    public class VandermondeEnumeratorTests
+    {
+        [TestMethod()]
+        public void VandermondeEnumeratorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetTest()
+        {
+            VandermondeEnumerator ve = new (2, 3, 8);
+            ve.MoveNext();
+            ve.MoveNext();
+            var ori_current = ve.Current;
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.Reset();
+            Assert.IsTrue(ve.MoveNext());
+            Assert.IsTrue(ve.MoveNext());
+            Assert.AreEqual(ve.Current, ori_current);
+        }
+
+        [TestMethod()]
+        public void MoveNextTest()
+        {
+            VandermondeEnumerator ve = new (2, 3, 8);
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.MoveNext();
+            ve.MoveNext();
+            Assert.IsFalse(ve.MoveNext());
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
