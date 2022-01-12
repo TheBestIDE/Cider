95bbb2d89beac23773ad1b67b59f121a516ed862
金路超 <785515163@qq.com>
2022-01-12 15:28:59 +0800

    测试数据层接口

---
+++ Cider.ServerTests/Data/DataProviderTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cider.Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cider.Server.Data.Tests
+{
+    [TestClass()]
+    public class DataProviderTests
+    {
+        protected DataProvider dataProvider = new DataProvider();
+
+        [TestMethod()]
+        public void FindNotExistHashTest()
+        {
+            List<string> hashs = new List<string>();
+            // 添加已有的hash
+            for (int i = 6; i < 10; i++)
+            {
+                hashs.Add("BlockHash" + i);
+            }
+            // 添加不存在的hash
+            for (int i = 20; i < 25; i ++)
+            {
+                hashs.Add("BlockHash" + i);
+            }
+
+            var diff = dataProvider.FindNotExistHash(hashs.ToArray());
+            Assert.IsNotNull(diff);
+            Assert.AreEqual(5, diff.Length);
+
+            for (int i = 0; i < diff.Length; i++)
+            {
+                Assert.AreEqual("BlockHash" + (i + 20), diff[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void SetBlocksMessageTest()
+        {
+            Dictionary<string, string> blocks = new Dictionary<string, string>();
+            for (int i = 100; i < 105; i++)
+            {
+                blocks["BlockHash" + i] = i.ToString();
+            }
+            dataProvider.SetBlocksMessage(blocks);
+
+            var db = Cider.Server.Core.Single<ServerDb>.Instance;
+            for (int i = 100; i < 105; i++)
+            {
+                var hash = "BlockHash" + i;
+                var block = db.GetBlock(hash);
+                Assert.IsNotNull(block);
+                Assert.AreEqual(blocks[hash], block.BlockFileName);
+            }
+        }
+
+        [TestMethod()]
+        public void ReadFileMessageTest()
+        {
+            var file = dataProvider.ReadFileMessage("Test");
+            Assert.IsNotNull(file);
+            Assert.AreEqual("Test", file.FileName);
+            Assert.IsNotNull(file.BlockHashList);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual("BlockHash" + i, file.BlockHashList[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void ReadFileBlocksFileNameTest()
+        {
+            var names = dataProvider.ReadFileBlocksFileName("Test");
+            Assert.IsNotNull(names);
+            for (int i = 0; i < names.Length; i++)
+            {
+                Assert.AreEqual(i.ToString(), names[i]);
+            }
+        }
+    }
+}
---
+++ Cider.ServerTests/Data/ServerDbTests.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cider.Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Cider.Server.Data.Model;
+
+namespace Cider.Server.Data.Tests
+{
+    [TestClass()]
+    public class ServerDbTests
+    {
+        [TestMethod()]
+        public void StaticServerDbTest()
+        {
+            string dbPath = "./" + ServerDb.DbFileDirectory + "/" + ServerDb.DbFileName;
+            Assert.IsTrue(File.Exists(dbPath));
+        }
+
+        [TestMethod()]
+        public void ServerDbTest()
+        {
+            using ServerDb db = new ServerDb();
+        }
+
+        [TestMethod()]
+        public void GetFileTest()
+        {
+            using var db = new ServerDb();
+            var rcvFile = db.GetFile("Test");
+            Assert.IsNotNull(rcvFile);
+            Assert.IsNotNull(rcvFile.BlockHash);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual("BlockHash" + i, rcvFile.BlockHash[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertFileTest()
+        {
+            using var db = new ServerDb();
+            Files? file = new Files()
+            {
+                FileName = "Test"
+            };
+
+            IList<string> hashs = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                hashs.Add("BlockHash" + i);
+            }
+            file.BlockHash = hashs;
+
+            db.InsertFile(file);
+
+            var rcvFile = db.GetFile(file.FileName);
+            Assert.IsNotNull(rcvFile);
+            Assert.IsNotNull(rcvFile.BlockHash);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(hashs[i], rcvFile.BlockHash[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void DeleteFileTest()
+        {
+            using var db = new ServerDb();
+            db.DeleteFile("Test");
+            var rcvFile = db.GetFile("Test");
+            Assert.IsNull(rcvFile);
+        }
+
+        [TestMethod()]
+        public void GetBlockTest()
+        {
+            using var db = new ServerDb();
+            for (int i = 0; i < 10; i++)
+            {
+                var block = db.GetBlock("BlockHash" + i);
+                Assert.IsNotNull(block);
+                Assert.AreEqual("BlockHash" + i, block.BlockHash);
+                Assert.AreEqual(i.ToString(), block.BlockFileName);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertBlockTest()
+        {
+            using var db = new ServerDb();
+            IList<FileBlocks> fileBlocks = new List<FileBlocks>();
+            for (int i = 0; i < 10; i++)
+            {
+                fileBlocks.Add(new FileBlocks()
+                {
+                    BlockHash = "BlockHash" + i,
+                    BlockFileName = i.ToString(),
+                });
+            }
+
+            foreach (var block in fileBlocks)
+            {
+                db.InsertBlock(block);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var block = db.GetBlock("BlockHash" + i);
+                Assert.IsNotNull(block);
+                Assert.AreEqual(fileBlocks[i].BlockHash, block.BlockHash);
+                Assert.AreEqual(fileBlocks[i].BlockFileName, block.BlockFileName);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertBlocksTest()
+        {
+            using var db = new ServerDb();
+            IList<FileBlocks> fileBlocks = new List<FileBlocks>();
+            for (int i = 0; i < 10; i++)
+            {
+                fileBlocks.Add(new FileBlocks()
+                {
+                    BlockHash = "BlockHash" + i,
+                    BlockFileName = i.ToString(),
+                });
+            }
+
+            db.InsertBlocks(fileBlocks);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var block = db.GetBlock("BlockHash" + i);
+                Assert.IsNotNull(block);
+                Assert.AreEqual(fileBlocks[i].BlockHash, block.BlockHash);
+                Assert.AreEqual(fileBlocks[i].BlockFileName, block.BlockFileName);
+            }
+        }
+
+        [TestMethod()]
+        public void DeleteBlockTest()
+        {
+            using var db = new ServerDb();
+            for (int i = 0; i < 10; i++)
+            {
+                db.DeleteBlock("BlockHash" + i);
+                Assert.IsNull(db.GetBlock("BlockHash" + i));
+            }
+        }
+
+        [TestMethod()]
+        public void DeleteBlocksTest()
+        {
+            using var db = new ServerDb();
+            IList<string> hashs = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                hashs.Add("BlockHash" + i);
+            }
+            db.DeleteBlocks(hashs);
+
+            foreach (var hash in hashs)
+            {
+                Assert.IsNull(db.GetBlock(hash));
+            }
+        }
+
+        [TestMethod()]
+        public void CombineFilesAndBlocksTest()
+        {
+            using var db = new ServerDb();
+            var hashFileName = db.CombineFilesAndBlocks("Test");
+            Assert.IsNotNull(hashFileName);
+            Assert.AreEqual(5, hashFileName.Length);
+            for (int i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(i.ToString(), hashFileName[i]);
+            }
+        }
+    }
+}
