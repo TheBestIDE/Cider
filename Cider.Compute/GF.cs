eded49b4063cd3ec4c7b46c22730a40c2dd6f0c7
金路超 <785515163@qq.com>
2022-01-15 12:52:21 +0800

    实现有限域上的带余除法和求逆运算并重载了除法运算符

--- Cider.Compute/GF.cs
+++ Cider.Compute/GF.cs
@@ -7,7 +7,13 @@ namespace Cider.Math
     /// </summary>
     public class GF
     {
+        #region Field
+
         private readonly LongBytes _dat;
+
+        #endregion
+
+        #region Property
 
         /// <summary>
         /// 不可约多项式字典
@@ -19,12 +25,16 @@ namespace Cider.Math
         /// </summary>
         public long BitLength { get; private set; }
 
+        #endregion
+
+        #region Constructor
+
         static GF()
         {
             PrimitiveDict = new Dictionary<long, LongBytes>
             {
                 [4] = 0b10011,     // x^4 + x + 1
-                [8] = (1UL << 8) | 0b11101,  // x^8 + x^4 + x^3 + x^2 + 1
+                [8] = 0b100011011, // x^8 + x^4 + x^3 + x^1 + 1
                 [16] = (1UL << 16) | (1 << 12) | 0b1011,     // x^16 + x^12 + x^3 + x + 1
                 [32] = (1UL << 32) | (1 << 22) | 0b111,      // x^32 + x^22 + x^2 + x + 1 
                 [64] = (1UL << 64) | 0b11011  // x^64 + x^4 + x^3 + x + 1
@@ -41,24 +51,44 @@ namespace Cider.Math
         {
         }
 
-        public GF(long bitnum, ulong b)
-        {
-            _dat = b;
-            BitLength = bitnum;
-        }
-
         public GF(LongBytes longBytes)
         {
             _dat = new LongBytes(longBytes);
             BitLength = longBytes.ByteLength << 3;
         }
 
+        public GF(long bitnum, LongBytes longBytes)
+        {
+            _dat = new LongBytes(longBytes);
+            BitLength = bitnum;
+        }
+
+        #endregion
+
         #region Method
+
+        #region Override
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+            if (obj is GF gf)
+                return (_dat == gf._dat) && (BitLength == gf.BitLength);
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return BitLength.GetHashCode() ^ _dat.GetHashCode();
+        }
 
         public override string ToString()
         {
             return _dat.ToString();
         }
+
+        #endregion
 
         /// <summary>
         /// 扩展欧几里得算法求乘法逆
@@ -66,20 +96,16 @@ namespace Cider.Math
         /// <returns></returns>
         public GF Inverse()
         {
-            var byteLength = BitLength >> 3;
-            LongBytes x1 = new(byteLength, 1);
-            LongBytes x2 = new(byteLength, 0);
-            LongBytes a = new LongBytes(this._dat);
-            LongBytes b = new (PrimitiveDict[BitLength]);
-            LongBytes zero = new(b.ByteLength);
+            GF x1 = One(BitLength);
+            GF x2 = Zero(BitLength);
+            GF a = new (BitLength, _dat);
+            GF b = new (BitLength, PrimitiveDict[BitLength]);
+            GF zero = Zero(b.BitLength);
             while (b != zero)
             {
-                GF q, r;
-                DivMod(a, b, out q, out r);
-                a = b;
-                b = r._dat;
-                x1 = x2;
-                x2 = x1 ^ q * x2;
+                DivMod(a, b, out GF q, out GF r);
+                (a, b) = (b, new (BitLength, r._dat));
+                (x1, x2) = (x2, x1 + q * x2);
             }
             return new GF(x1);
         }
@@ -92,33 +118,53 @@ namespace Cider.Math
         /// <returns></returns>
         public static GF Zero(long bitnum)
         {
-            return new GF(bitnum, 0x0);
+            return new GF(bitnum);
         }
 
         /// <summary>
         /// 获取有限域上的乘法单位元
         /// </summary>
         /// <returns></returns>
-        public static GF One(uint bitnum)
+        public static GF One(long bitnum)
         {
             return new GF(bitnum, 0x1);
         }
 
+        /// <summary>
+        /// 有限域上的加法
+        /// <para>即按位求异或</para>
+        /// </summary>
+        /// <returns>和</returns>
+        /// <exception cref="ArgumentException"></exception>
         public static GF Add(GF left, GF right)
         {
             if (left.BitLength != right.BitLength)
                 throw new ArgumentException("The left and right is not in the same Galois Field");
-            ulong res = (ulong)(left._dat ^ right._dat);
+            var res = left._dat ^ right._dat;
             return new GF(left.BitLength, res);
         }
 
+        /// <summary>
+        /// 有限域上的减法
+        /// <para>实际上与加法相同</para>
+        /// </summary>
+        public static GF Subtract(GF left, GF right)
+        {
+            return Add(left, right);
+        }
+
+        /// <summary>
+        /// 有限域上的乘法
+        /// </summary>
+        /// <returns>积</returns>
+        /// <exception cref="ArgumentException"></exception>
         public static GF Multify(GF left, GF right)
         {
             if (left.BitLength != right.BitLength)
                 throw new ArgumentException("The left and right is not same Field");
             LongBytes l_bits = left._dat, r_bits = right._dat;
             long len = left.BitLength;
-            var result = new LongBytes(left.BitLength >> 3);
+            LongBytes result = new (left._dat.ByteLength);
             // 使用快速平方乘算法计算left*right
             while (r_bits != 0)
             {
@@ -127,14 +173,32 @@ namespace Cider.Math
                     result ^= l_bits;
                 }
 
-                LongBytes flag = l_bits & (new LongBytes(left.BitLength >> 3, 1UL) << (int)(len - 1));   // 记录最高位是否为1
+                LongBytes flag = l_bits & (new LongBytes(left._dat.ByteLength, 1UL) << (int)(len - 1));   // 记录最高位是否为1
                 l_bits <<= 1;
                 if (flag != 0)
                     l_bits ^= PrimitiveDict[len];   // 左移后溢出 需要减去不可约多项式以回到域内
                 r_bits >>= 1;
             }
 
-            return new GF(result);
+            return new GF(len, result);
+        }
+
+        /// <summary>
+        /// 使用乘法逆元计算有限域上的除法
+        /// <para>被除数乘上除数的乘法逆</para>
+        /// </summary>
+        /// <param name="left">被除数</param>
+        /// <param name="right">除数</param>
+        /// <returns>商</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="DivideByZeroException"></exception>
+        public static GF Divide(GF left, GF right)
+        {
+            if (left.BitLength != right.BitLength)
+                throw new ArgumentException("The left and right is not same Field");
+            if (right == Zero(right.BitLength))
+                throw new DivideByZeroException();
+            return left * right.Inverse();
         }
 
         /// <summary>
@@ -153,7 +217,7 @@ namespace Cider.Math
             if (divisor == Zero(divisor.BitLength))
                 throw new DivideByZeroException();
 
-            long byteLength = dividend.BitLength >> 3;
+            long byteLength = dividend._dat.ByteLength;
             LongBytes qtnt = new(byteLength);      // 商
             LongBytes rmdr = new(dividend._dat);   // 余数
             LongBytes one = new(byteLength, 1);    // 长字节类型1
@@ -167,25 +231,25 @@ namespace Cider.Math
                 qtnt |= one << (int)rec;
                 digit_dividend = rmdr.HighestBitPosition;
             }
-            quotient = new GF(qtnt);
-            remainder = new GF(rmdr);
+            quotient = new GF(dividend.BitLength, qtnt);
+            remainder = new GF(dividend.BitLength, rmdr);
         }
 
         #region Type Conversion
 
         public static implicit operator GF(byte b)
         {
-            return new GF(8, b);
+            return new GF(8, (ulong)b);
         }
 
         public static implicit operator GF(ushort b)
         {
-            return new GF(16, b);
+            return new GF(16, (ulong)b);
         }
 
         public static implicit operator GF(uint b)
         {
-            return new GF(32, b);
+            return new GF(32, (ulong)b);
         }
 
         public static implicit operator GF(ulong b)
@@ -196,11 +260,6 @@ namespace Cider.Math
         public static implicit operator GF(LongBytes longBytes)
         {
             return new GF(longBytes);
-        }
-
-        public static implicit operator LongBytes(GF gf)
-        {
-            return new LongBytes(gf._dat);
         }
 
         public static implicit operator byte[](GF gf)
@@ -247,6 +306,11 @@ namespace Cider.Math
             return (ulong)gf._dat;
         }
 
+        public static explicit operator LongBytes(GF gf)
+        {
+            return new LongBytes(gf._dat);
+        }
+
         #endregion
 
         #region Operator Overloading
@@ -285,6 +349,11 @@ namespace Cider.Math
         public static GF operator*(GF left, GF right)
         {
             return Multify(left, right);
+        }
+
+        public static GF operator/(GF left, GF right)
+        {
+            return Divide(left, right);
         }
 
         #endregion
---
+++ Cider.ServerTests/Math/GFTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cider.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cider.Math.Tests
+{
+    [TestClass()]
+    public class GFTests
+    {
+        [TestMethod()]
+        public void GFTest()
+        {
+            LongBytes num = 0xFF1F;
+            GF gf = new GF(num);
+            Assert.AreEqual(64, gf.BitLength);
+        }
+
+        [TestMethod()]
+        public void InverseTest1()
+        {
+            GF gf = new GF(8, 0x8C), exp = new GF(8, 0xF7);
+            Assert.AreEqual(exp.ToString(), gf.Inverse().ToString());
+
+            gf = new GF(8, 0xBE); exp = new GF(8, 0x86);
+            Assert.AreEqual(exp.ToString(), gf.Inverse().ToString());
+
+            gf = new GF(8, 0x01); exp = new GF(8, 0x01);
+            Assert.AreEqual(exp.ToString(), gf.Inverse().ToString());
+
+            gf = new GF(8, 0x2D); exp = new GF(8, 0x44);
+            Assert.AreEqual(exp.ToString(), gf.Inverse().ToString());
+        }
+
+        [TestMethod()]
+        public void InverseTest2()
+        {
+            GF gf = new GF(8, 0x8C), one = GF.One(8);
+            Assert.AreEqual(one.ToString(), (gf * gf.Inverse()).ToString());
+
+            gf = new GF(8, 0xBE);
+            Assert.AreEqual(one.ToString(), (gf * gf.Inverse()).ToString());
+
+            gf = new GF(8, 0x01);
+            Assert.AreEqual(one.ToString(), (gf * gf.Inverse()).ToString());
+
+            gf = new GF(8, 0x2D);
+            Assert.AreEqual(one.ToString(), (gf * gf.Inverse()).ToString());
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            GF gf1 = 0x89;
+            GF gf2 = 0x4D;
+            GF expected = 0xC4;
+            Assert.IsTrue(expected == gf1 + gf2);
+
+            gf1 = 0xAF; gf2 = 0x3B; expected = 0x94;
+            Assert.IsTrue(expected == gf1 + gf2);
+
+            gf1 = 0x35; gf2 = 0xC6; expected = 0xF3;
+            Assert.IsTrue(expected == gf1 + gf2);
+        }
+
+        [TestMethod()]
+        public void MultifyTest()
+        {
+            GF gf1 = new (8, 0xCE), gf2 = new (8, 0xF1), expected = new (8, 0xEF);
+            Assert.IsTrue(expected == gf1 * gf2);
+
+            gf1 = new GF(8, 0x70); gf2 = new GF(8, 0x99); expected = new GF(8, 0xA2);
+            Assert.IsTrue(expected == gf1 * gf2);
+
+            gf1 = new GF(8, 0x00); gf2 = new GF(8, 0xA4); expected = new GF(8, 0x00);
+            Assert.IsTrue(expected == gf1 * gf2);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            GF gf = new GF(8, 0x8C), one = GF.One(8);
+            Assert.AreEqual(one.ToString(), (gf / gf).ToString());
+
+            gf = new GF(8, 0xBE);
+            Assert.AreEqual(one.ToString(), GF.Divide(gf, gf).ToString());
+
+            gf = new GF(8, 0x01);
+            Assert.AreEqual(one.ToString(), (gf / gf).ToString());
+
+            gf = new GF(8, 0x2D);
+            Assert.AreEqual(one.ToString(), GF.Divide(gf, gf).ToString());
+        }
+
+        [TestMethod()]
+        public void DivModTest()
+        {
+            GF gf1 = new(8, 0xDE), gf2 = new(8, 0xC6), expQtnt = new GF(8, 0x01), expRmd = new GF(8, 0x18);
+            GF qtnt, rmd;
+            GF.DivMod(gf1, gf2, out qtnt, out rmd);
+            Assert.AreEqual(expQtnt.ToString(), qtnt.ToString());
+            Assert.AreEqual(expRmd.ToString(), rmd.ToString());
+
+            gf1 = new(8, 0x8C); gf2 = new GF(8, 0x0A); expQtnt = new GF(8, 0x14); expRmd = new GF(8, 0x04);
+            GF.DivMod(gf1, gf2, out qtnt, out rmd);
+            Assert.AreEqual(expQtnt.ToString(), qtnt.ToString());
+            Assert.AreEqual(expRmd.ToString(), rmd.ToString());
+
+            gf1 = new(8, 0x3E); gf2 = new GF(8, 0xA4); expQtnt = new GF(8, 0x00); expRmd = new GF(8, 0x3E);
+            GF.DivMod(gf1, gf2, out qtnt, out rmd);
+            Assert.AreEqual(expQtnt.ToString(), qtnt.ToString());
+            Assert.AreEqual(expRmd.ToString(), rmd.ToString());
+        }
+    }
+}
